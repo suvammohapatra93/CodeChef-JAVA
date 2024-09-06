7f89194a8543705854b76492b500468173caa796
suvammohapatra93 <suvammohapatra93480@gmail.com>
2024-09-06 19:06:24 +0530

    Date-:06.09.24

--- Small_Factorials.java
+++ Small_Factorials.java
@@ -18,35 +18,44 @@ Output
 6
 */
 
-
-
-
 import java.util.*;
 import java.math.BigInteger;
+
 public class Small_Factorials
 {
-public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt(); // Read the number of test cases
-		
-		for (int i = 0; i < T; i++) {
-			int N = sc.nextInt(); // Read the number for which factorial is to be calculated
-			
-			// Use BigInteger to handle large factorials
-			BigInteger factorial = BigInteger.ONE;
-			
-			// Calculate factorial iteratively
-			for (int j = 2; j <= N; j++) {
-				factorial = factorial.multiply(BigInteger.valueOf(j));
-			}
-			
-			// Print the factorial
-			System.out.println(factorial);
+
+	// Method to compute factorial of a given number n
+	public static BigInteger factorial(int n) {
+		BigInteger result = BigInteger.ONE;
+		for (int i = 2; i <= n; i++) {
+			result = result.multiply(BigInteger.valueOf(i));
 		}
-		sc.close();
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Read number of test cases
+		int t = scanner.nextInt();
+
+		// Array to store results
+		BigInteger[] results = new BigInteger[t];
+
+		// Read each number and compute its factorial
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			results[i] = factorial(n);
+		}
+
+		// Print each factorial result
+		for (BigInteger result : results) {
+			System.out.println(result);
+		}
+
+		scanner.close();
 	}
 }
 
-//! Time Complexity = O(T)
-//! Space Complexity = O(1)
+// ! Time Complexity = O(T)
+// ! Space Complexity = O(1)
