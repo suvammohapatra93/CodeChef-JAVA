2fec4c26212ce83d54063204241fdea226e331c5
suvammohapatra93 <suvammohapatra93480@gmail.com>
2024-10-24 05:59:32 +0530

    Date->24-10-24

---
+++ ProgrammingLanguages.java
@@ -0,0 +1,28 @@
+
+//CodeChef -> Programming Languages:
+
+import java.util.*;
+
+public class ProgrammingLanguages {
+    public static void main(String[] args) throws java.lang.Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int a1 = sc.nextInt();
+            int b1 = sc.nextInt();
+            int a2 = sc.nextInt();
+            int b2 = sc.nextInt();
+            if ((a == a1 || a == b1) && (b == a1 || b == b1)) {
+                System.out.println(1);
+            } else if ((a == a2 || a == b2) && (b == a2 || b == b2)) {
+                System.out.println(2);
+            } else {
+                System.out.println(0);
+            }
+        }
+        sc.close();
+    }
+}
